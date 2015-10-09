de4aad6e1b40b3c4481f7ee1dfb66d046196cad2
Luke McGregor <luke@mcgregor.org.nz>
2015-10-09 14:21:33 +1300

    Deploy as well as trim

--- NugetPackageTrimmer/Program.cs
+++ NugetPackageTrimmer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using NuGet;
 
@@ -8,25 +9,41 @@ namespace NugetPackageTrimmer
 	{
 		static void Main(string[] args)
 		{
-			var repoUrl = args.Length > 2 ? args[1] : "https://packages.nuget.org/api/v2";
+			var repoUrl = args.Length > 3 ? args[2] : "https://packages.nuget.org/api/v2";
 
 			Console.WriteLine($"Checking if packages in [{args[0]}] exist in {repoUrl} and deleting any that dont need deploying...");
 
 			var repo = PackageRepositoryFactory.Default.CreateRepository(repoUrl);
 
+			var packageServer = new PackageServer(repoUrl, "NuGet Command Line");
+			packageServer.SendingRequest += (sender, e) =>
+			{
+				Console.WriteLine(String.Format(CultureInfo.CurrentCulture, "{0} {1}", e.Request.Method, e.Request.RequestUri));
+			};
+
 			foreach (var nupkg in Directory.EnumerateFiles(args[0], "*.nupkg"))
 			{
 				Console.WriteLine($"Checking nuget package : {nupkg}");
-				var package = new ZipPackage(nupkg);
+				var package = new OptimizedZipPackage(nupkg);
 
 				if (repo.Exists(package))
 				{
-					Console.WriteLine("That package already exists, deleting...");
+					Console.WriteLine("That package already exists, skipping...");
 					File.Delete(nupkg);
 				}
 				else
 				{
-					Console.WriteLine("That package isnt in nuget yet, skipping...");
+					Console.WriteLine("That package isnt in nuget yet, pushing...");
+					// Push the package to the server
+					var sourceUri = new Uri(repoUrl);
+
+					packageServer.PushPackage(
+						args[1],
+						package,
+						new FileInfo(nupkg).Length,
+						Convert.ToInt32(new TimeSpan(0, 0, 1, 0, 0).TotalMilliseconds),
+						false);
+					Console.WriteLine("Pushed");
 				}
 			}
 		}
