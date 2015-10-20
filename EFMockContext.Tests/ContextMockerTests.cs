f46bdbc464099986633fd36203af17ad1fb4a0c9
Luke McGregor <luke@mcgregor.org.nz>
2015-10-20 15:12:05 +1300

    Mock Context

---
+++ EFMockContext.Tests/ContextMockerTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.Entity;
+using FakeDbSet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Smartrak.EFMockContext.Tests
+{
+	public interface ITestContext
+	{
+		IDbSet<Entity> Entity1s { get; set; }
+		IDbSet<Entity> Entity2s { get; set; }
+		int SaveChanges();
+	}
+
+	public class Entity
+	{
+		public int Id { get; set; }
+	}
+
+	[TestClass]
+	public class ContextMockerTests
+	{
+		[TestMethod]
+		public void TestPropertiesCreated()
+		{
+			var sut = ContextMocker.CreateMockContext<ITestContext>();
+
+
+			Assert.IsInstanceOfType(sut.Entity1s, typeof(InMemoryDbSet<Entity>));
+			Assert.IsInstanceOfType(sut.Entity2s, typeof(InMemoryDbSet<Entity>));
+			Assert.AreNotEqual(sut.Entity1s, sut.Entity2s);
+		}
+
+		[TestMethod]
+		public void SaveChangesDoesntFail()
+		{
+			var sut = ContextMocker.CreateMockContext<ITestContext>();
+
+			var res = sut.SaveChanges();
+
+			Assert.AreEqual(0, res);
+		}
+	}
+}
---
+++ EFMockContext/ContextMocker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using Moq;
+
+namespace Smartrak.EFMockContext
+{
+	public static class ContextMocker
+	{
+		public static T CreateMockContext<T>(bool clearDownExistingData = false) where T : class
+		{
+			var mockContext = new Mock<T>();
+			var parameter = Expression.Parameter(typeof(T));
+
+			foreach (var iDbSetProperty in typeof(T).GetProperties().Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(IDbSet<>)))
+			{
+				var body = Expression.PropertyOrField(parameter, iDbSetProperty.Name);
+				var lambdaExpression = Expression.Lambda<Func<T, object>>(body, parameter);
+				mockContext.SetupProperty(lambdaExpression, Activator.CreateInstance(typeof(FakeDbSet.InMemoryDbSet<>).MakeGenericType(iDbSetProperty.PropertyType.GenericTypeArguments), args: clearDownExistingData));
+			}
+
+			return mockContext.Object;
+		}
+	}
+}
