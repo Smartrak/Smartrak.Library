5bbf83e7d926f296b0d0e51879bdaf18df22d231
Luke McGregor <luke@mcgregor.org.nz>
2015-10-28 11:28:40 +1300

    Added stream read all lines function

---
+++ Collections/Streams/StreamHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Smartrak.Collections.Streams
+{
+	/// <summary>
+	/// Reads all lines from a stream as an enumerable.
+	/// Adapted from @jonskeet's answer here: http://stackoverflow.com/a/13312954/1070291
+	/// </summary>
+	public static class StreamHelpers
+	{
+		public static IEnumerable<string> ReadLines(this Func<Stream> streamProvider, Encoding encoding)
+		{
+			using (var stream = streamProvider())
+			using (var reader = new StreamReader(stream, encoding))
+			{
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					yield return line;
+				}
+			}
+		}
+	}
+}
