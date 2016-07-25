f76a96fb2e32317224983d7a3976ae4b8a22dc42
Kerry <kerryarts@users.noreply.github.com>
2016-07-25 14:17:21 +1200

    Add IEnumerableExtensions, which includes Aggregate() and ToHashSet()

---
+++ System.Contrib/Linq/IEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace System.Contrib.Linq
+{
+	// ReSharper disable InconsistentNaming
+	public static class IEnumerableExtensions
+	// ReSharper restore InconsistentNaming
+	{
+		public static TAggregate Aggregate<TAggregate, TSource>(this IEnumerable<TSource> source, TAggregate seed, Action<TAggregate, TSource> accumulator)
+		{
+			foreach (var item in source)
+				accumulator.Invoke(seed, item);
+
+			return seed;
+		}
+
+		public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
+		{
+			return new HashSet<T>(source);
+		}
+	}
+}
