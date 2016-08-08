31f3bbcfdfab08cc65ecfb3e40040ef9f618e5b2
Nathan Robb <nathanrobb@hotmail.com>
2016-08-09 09:08:53 +1200

    Bump nuget id to keep future versions working
    - Previous version has an updated moq nuget version that is no longer
    required.
    - This version is functionally equivalent to v1.0.2

--- FakeDbSet.ContextGenerator/Properties/AssemblyInfo.cs
+++ FakeDbSet.ContextGenerator/Properties/AssemblyInfo.cs
@@ -31,6 +31,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyVersion(AssemblyMeta.Version)]
+[assembly: AssemblyFileVersion(AssemblyMeta.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyMeta.Version)]
+
+internal static class AssemblyMeta
+{
+	public const string Version = "1.0.4";
+}
