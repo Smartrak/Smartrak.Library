c6b91daeaeb4778b5e6cffaa2dcc1dabd234cd92
Kerry <kerryarts@users.noreply.github.com>
2016-08-24 16:01:16 +1200

    Add Min/Max extensions for IComparable

---
+++ System.Contrib/Comparable/ComparableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Contrib.Comparable
+{
+	public static class ComparableExtensions
+	{
+		public static T Min<T>(this T a, T b) where T : IComparable<T>
+		{
+			return a.CompareTo(b) > 0 ? b : a;
+		}
+
+		public static T Max<T>(this T a, T b) where T : IComparable<T>
+		{
+			return a.CompareTo(b) > 0 ? a : b;
+		}
+	}
+}
