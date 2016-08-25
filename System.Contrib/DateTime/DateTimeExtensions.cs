d95700892aeec74dc896a41af38fab03b2b9c850
Steven Crake <steven@smartrak.co.nz>
2016-08-26 10:59:37 +1200

    Add a TimeSpanExtensions class with .Average
    Tested in TimeSpanExtensionsTests in System.Contrib.Tests.

--- System.Contrib/DateTime/DateTimeExtensions.cs
+++ System.Contrib/DateTime/DateTimeExtensions.cs
@@ -9,13 +9,13 @@ namespace System.Contrib.DateTime
 			return date.AddDays(System.DateTime.DaysInMonth(date.Year, date.Month) - date.Day);
 		}
 
-		public static IEnumerable<System.DateTime> Range(this System.DateTime startDate, System.DateTime endDateExclusive, TimeSpan interval)
+		public static IEnumerable<System.DateTime> Range(this System.DateTime startDate, System.DateTime endDateExclusive, System.TimeSpan interval)
 		{
 			for (var d = startDate; d <= endDateExclusive; d += interval)
 				yield return d;
 		}
 
-		public static IEnumerable<System.DateTime> Range(this System.DateTime startDate, TimeSpan duration, TimeSpan interval)
+		public static IEnumerable<System.DateTime> Range(this System.DateTime startDate, System.TimeSpan duration, System.TimeSpan interval)
 		{
 			return startDate.Range(startDate.Add(duration), interval);
 		}
@@ -23,7 +23,7 @@ namespace System.Contrib.DateTime
 		public static IEnumerable<System.DateTime> RangeOverDays(this System.DateTime startDate, int days)
 		{
 			// Subtract 1 from the days, as it is counter intuitive to request the range over X days, but have X+1 DateTimes returned
-			return startDate.Range(startDate.AddDays(days - 1), TimeSpan.FromDays(1));
+			return startDate.Range(startDate.AddDays(days - 1), System.TimeSpan.FromDays(1));
 		}
 	}
 }
