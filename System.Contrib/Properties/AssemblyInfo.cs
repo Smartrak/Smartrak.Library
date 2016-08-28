f3af6712019ee68bff512fd4b68aeb54d83e7b74
Matt Stannett <matt.stannett@gmail.com>
2016-08-29 11:53:03 +1200

    Fixed csproj to include contrib

--- System.Contrib/Properties/AssemblyInfo.cs
+++ System.Contrib/Properties/AssemblyInfo.cs
@@ -21,22 +21,12 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ac97934a-bbbc-488b-b337-a11bf4c98e97")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion(AssemblyMeta.Version)]
 [assembly: AssemblyFileVersion(AssemblyMeta.Version)]
 [assembly: AssemblyInformationalVersion(AssemblyMeta.Version)]
 
 internal static class AssemblyMeta
 {
-	public const string Version = "1.12.0";
+	public const string Version = "1.12.1";
 }
 
