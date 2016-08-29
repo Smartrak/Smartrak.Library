4e78e6a5d136850919ebadc5a4a37d4b69efc42f
Matt Stannett <matt.stannett@gmail.com>
2016-08-29 13:08:22 +1200

    added extension for getting clientIP in webapi

---
+++ System.Contrib/Net/Http/ClientIp.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+
+namespace System.Net.Http.Contrib
+{
+	public static class ClientIp
+	{
+		/// <summary>
+		/// Gets the IP address of the client in webapi, based on http://www.herlitz.nu/2013/06/27/getting-the-client-ip-via-asp-net-web-api/
+		/// </summary>
+		/// <param name="request">The request to examine</param>
+		/// <returns>A string version of the IP of the client or null if not found.</returns>
+		public static string GetClientIpAddress(this HttpRequestMessage request)
+		{
+			if (request.Properties.ContainsKey("MS_HttpContext"))
+			{
+				return ((HttpContextWrapper)request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+			}
+			return null;
+		}
+	}
+}
--- System.Contrib/Properties/AssemblyInfo.cs
+++ System.Contrib/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 
 internal static class AssemblyMeta
 {
-	public const string Version = "1.12.1";
+	public const string Version = "1.13.0";
 }
 
