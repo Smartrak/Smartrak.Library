66f38e55c4c1f02c6a63fd63f73517cc4d51d003
Steven Crake <steven@smartrak.co.nz>
2016-09-09 15:02:53 +1200

    Missed in last commit :(

--- LibPhoneNumber.Contrib/PhoneNumberUtil/PhoneNumberValidator.cs
+++ LibPhoneNumber.Contrib/PhoneNumberUtil/PhoneNumberValidator.cs
@@ -1,29 +1,27 @@
-﻿using PhoneNumbers;
-
-namespace LibPhoneNumber.Contrib.PhoneNumberUtil
+﻿namespace LibPhoneNumber.Contrib.PhoneNumberUtil
 {
 	public static class PhoneNumberValidator
 	{
 		public static bool IsValid(string numberString, string[] regionCodes)
 		{
-			PhoneNumber phoneNumber;
+			PhoneNumbers.PhoneNumber phoneNumber;
 			return PhoneNumbers.PhoneNumberUtil.GetInstance().TryGetValidNumber(numberString, regionCodes, out phoneNumber);
 		}
 
 		/// <returns>Null for invalid input</returns>
-		public static PhoneNumber GetValidPhoneNumber(string numberString, string[] regionCodes)
+		public static PhoneNumbers.PhoneNumber GetValidPhoneNumber(string numberString, string[] regionCodes)
 		{
 			return PhoneNumbers.PhoneNumberUtil.GetInstance().GetValidNumber(numberString, regionCodes);
 		}
 
 		public static bool IsValidMobileNumber(string numberString, string[] regionCodes)
 		{
-			PhoneNumber phoneNumber;
+			PhoneNumbers.PhoneNumber phoneNumber;
 			return PhoneNumbers.PhoneNumberUtil.GetInstance().TryGetValidMobileNumber(numberString, regionCodes, out phoneNumber);
 		}
 
 		/// <returns>Null for invalid input</returns>
-		public static PhoneNumber GetValidMobileNumber(string numberString, string[] regionCodes)
+		public static PhoneNumbers.PhoneNumber GetValidMobileNumber(string numberString, string[] regionCodes)
 		{
 			return PhoneNumbers.PhoneNumberUtil.GetInstance().GetValidMobileNumber(numberString, regionCodes);
 		}
