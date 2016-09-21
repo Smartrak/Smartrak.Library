efded29b0e9e60db337b1ef4956802715a6812e2
Orangeyness <reuben_bell@hotmail.com>
2016-09-21 16:07:39 +1200

    Added ValidateCollectionAttribute

---
+++ DataAnnotations.Contrib/ValidateCollectionAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotations.Contrib
+{
+	public class ValidateCollectionAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			var collection = value as ICollection;
+
+			if (collection == null)
+				return ValidationResult.Success;
+
+			foreach (var obj in collection)
+			{
+				var results = new List<ValidationResult>();
+				var context = new ValidationContext(obj, null, null);
+
+				Validator.TryValidateObject(obj, context, results, true);
+
+				if (results.Count > 0)
+					return results[0];
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
