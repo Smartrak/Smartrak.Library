bcc4ae4993bfedf3d3a5940a36330026e16d99ed
Orangeyness <reuben_bell@hotmail.com>
2016-09-21 13:32:08 +1200

    Add ValidateObjectAttribute

---
+++ DataAnnotations.Contrib/ValidateObjectAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace DataAnnotations.Contrib
+{
+	public class ValidateObjectAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			var results = new List<ValidationResult>();
+			var context = new ValidationContext(value, null, null);
+
+			Validator.TryValidateObject(value, context, results, true);
+
+			if (results.Count > 0)
+				return results[0];
+
+			return ValidationResult.Success;
+		}
+	}
+}
