d073fe437906bf5e609a994b5a23a9662cbba29d
Steven Crake <steven@smartrak.co.nz>
2016-11-02 15:47:27 +1300

    Add EquivalentTo extension method for IEnumerable<T>

---
+++ System.Contrib.Tests/IEnumerableExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Contrib.Linq;
+using NUnit.Framework;
+
+namespace System.Contrib.Tests
+{
+	[TestFixture]
+	// ReSharper disable InconsistentNaming
+	internal class IEnumerableExtensionsTests
+	// ReSharper restore InconsistentNaming
+	{
+		#region Equivalent
+
+		[Test]
+		[TestCaseSource(nameof(GetEquivalentTestCases))]
+		public void ListSourceListOtherEquivalent(IEnumerable<int> source, IEnumerable<int> other)
+		{
+			Assert.True(source.EquivalentTo(other));
+		}
+
+		[Test]
+		[TestCaseSource(nameof(GetUnequivalentTestCases))]
+		public void ListSourceListOtherUnequivalent(IEnumerable<int> source, IEnumerable<int> other)
+		{
+			Assert.False(source.EquivalentTo(other));
+		}
+
+		#endregion
+
+		public static IEnumerable<object[]> GetEquivalentTestCases()
+		{
+			yield return new object[] { new int[0], new int[0] };
+			yield return new object[] { new[] { 1, 2, 2, 3, 4, 5, 1 }, new[] { 1, 5, 4, 3, 2, 2, 1 } };
+			yield return new object[] { new List<int> { 1, 2, 2, 3, 4, 5, 1 }, new List<int> { 1, 5, 4, 3, 2, 2, 1 } };
+			yield return new object[] { new HashSet<int> { 1, 2, 2, 3, 4, 5, 1 }, new HashSet<int> { 1, 5, 4, 3, 2, 2, 1 } };
+			yield return new object[] { new HashSet<int> { 1, 1, 1, 1 }, new HashSet<int> { 1 } };
+		}
+
+		public static IEnumerable<object[]> GetUnequivalentTestCases()
+		{
+			yield return new object[] { new[] { 1 }, new int[0] };
+			yield return new object[] { new[] { 1, 2 }, new [] { 1 } };
+			yield return new object[] { new List<int> { 1, 2, 2, 3 }, new List<int> { 1, 2, 3 } };
+			yield return new object[] { new List<int> { 1, 2, 3 }, new List<int> { 1, 2, 2, 3 } };
+			yield return new object[] { new HashSet<int> { 1, 2, 3, 1 }, new HashSet<int> { 1, 2, 3, 4 } };
+		}
+	}
+}
--- System.Contrib/Linq/IEnumerableExtensions.cs
+++ System.Contrib/Linq/IEnumerableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace System.Contrib.Linq
 {
@@ -17,6 +18,18 @@ namespace System.Contrib.Linq
 		public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
 		{
 			return new HashSet<T>(source);
+		}
+
+		/// <summary>
+		/// Checks that this collection has the same contents as another, ignoring order. Duplicates existing in one must exist in the other to be equivalent
+		/// </summary>
+		public static bool EquivalentTo<T>(this IEnumerable<T> source, IEnumerable<T> other)
+		{
+			// If expected implements ICollection, use it -- otherwise convert to array.
+			var sourceCopy = source as ICollection<T> ?? source.ToArray();
+			var otherCopy = new List<T>(other);
+
+			return sourceCopy.Count == otherCopy.Count && sourceCopy.All(otherCopy.Remove) && otherCopy.Count == 0;
 		}
 	}
 }
