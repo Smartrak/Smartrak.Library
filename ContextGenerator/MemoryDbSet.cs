2aa9a6214e77e46f7354cc65fb2be7692531201f
JeremyMooreSmartrak <jeremy@smartrak.co.nz>
2016-11-17 17:15:14 +1300

    Made ContextGenerator to replace FakeDbSet.ContextGenerator.
    This library works better when running tests in parallel because it doesn't use a static HashSet to store data.

---
+++ ContextGenerator/MemoryDbSet.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ContextGenerator
+{
+	public class MemoryDbSet<T> : IDbSet<T> where T : class
+	{
+		private readonly HashSet<T> _data;
+		private readonly IQueryable _query;
+
+		public MemoryDbSet()
+		{
+			_data = new HashSet<T>();
+			_query = _data.AsQueryable();
+		}
+
+		public Type ElementType
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public Expression Expression => _query.Expression;
+
+		public ObservableCollection<T> Local
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public IQueryProvider Provider => _query.Provider;
+
+		public T Add(T entity)
+		{
+			_data.Add(entity);
+			return entity;
+		}
+
+		public T Attach(T entity)
+		{
+			_data.Add(entity);
+			return entity;
+		}
+
+		public T Create()
+		{
+			throw new NotImplementedException();
+		}
+
+		public TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, T
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Find(params object[] keyValues)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return _data.GetEnumerator();
+		}
+
+		public T Remove(T entity)
+		{
+			_data.Remove(entity);
+			return entity;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return _data.GetEnumerator();
+		}
+	}
+}
