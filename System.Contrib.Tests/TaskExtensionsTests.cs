837ec7639ad3261b8085369939b1181435f8a1ca
Luke McGregor <luke.mcgregor@smartrak.co.nz>
2017-02-21 16:24:47 +1300

    Task extensions

---
+++ System.Contrib.Tests/TaskExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Contrib;
+
+namespace System.Contrib.Tests
+{
+	[TestFixture]
+	public class TaskExtensionsTests
+	{
+		[Test]
+		public void LongTaskTimesOut()
+		{
+			var task = Task.Run(() => { Thread.Sleep(1000); return 1; });
+
+			Assert.Throws<AggregateException>(() => task.TimeoutAfter(new System.TimeSpan(1)).Wait());
+		}
+
+		private static int _finishedTaskTimesDoesntExecuteAgainCounter = 0;
+		[Test]
+		public void FinishedTaskTimesDoesntExecuteAgain()
+		{
+			var task = Task.Run(() => { _finishedTaskTimesDoesntExecuteAgainCounter++; return 1; });
+
+			Thread.Sleep(10);
+			task.TimeoutAfter(new System.TimeSpan(1)).Wait();
+
+			Assert.AreEqual(1, _finishedTaskTimesDoesntExecuteAgainCounter);
+		}
+
+		[Test]
+		public void TaskCompletesWithoutReachingTimeout()
+		{
+
+			var task = Task.Run(() => { Thread.Sleep(50); return 1; });
+			var sw = Stopwatch.StartNew();
+			task.TimeoutAfter(new System.TimeSpan(0, 0, 0, 0, 10000)).Wait();
+			sw.ElapsedMilliseconds.Should().BeLessThan(100);
+		}
+	}
+}
---
+++ System.Contrib/Threading/Tasks/TaskExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace System.Threading.Tasks.Contrib
+{
+	/// <summary>
+	/// Extensions on top of System.Threading.Tasks.Task
+	/// </summary>
+	public static class TaskExtensions
+	{
+		/// <summary>
+		/// Waits for a task for a maximum time and then throws an exception. Useful if you don't want to wait forever for a hanging task.
+		/// NOTE: this does not cancel the inner task if it doesn't complete on time but it does return control.
+		/// From: http://stackoverflow.com/a/22078975/1070291
+		/// </summary>
+		/// <typeparam name="TResult">The result type of the subject task</typeparam>
+		/// <param name="task">the subject task</param>
+		/// <param name="timeout">The maximum time you want to wait before giving up on the task</param>
+		/// <returns>The result of the subject task</returns>
+		public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+		{
+			var timeoutCancellationTokenSource = new CancellationTokenSource();
+
+			var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+			if (completedTask == task)
+			{
+				timeoutCancellationTokenSource.Cancel();
+				return await task;
+			}
+			else
+			{
+				throw new TimeoutException("The operation has timed out.");
+			}
+		}
+	}
+}
