aef7d1ae95bfb3e8ccbce3f18670ad3681104f6c
Luke McGregor <luke.mcgregor@smartrak.co.nz>
2017-03-28 11:05:53 +1300

    Added test cases

--- System.Contrib.Tests/ParseEnumTests.cs
+++ System.Contrib.Tests/ParseEnumTests.cs
@@ -6,13 +6,24 @@ namespace System.Contrib.Tests
 	[TestFixture]
 	public class ParseEnumTests
 	{
-		[TestCase("qq", null)]
-		[TestCase("AThing", TestEnum.AThing)]
-		[TestCase("athing", TestEnum.AThing)]
-		[TestCase("anotherthing", TestEnum.AnOtherThing)]
-		public void EnumTest(string value, TestEnum? expected)
+		[TestCase("qq", null, null)]
+		[TestCase("AThing", null, TestEnum.AThing)]
+		[TestCase("athing", null, TestEnum.AThing)]
+		[TestCase("anotherthing", null, TestEnum.AnOtherThing)]
+		[TestCase("qq", TestEnum.AThing, TestEnum.AThing)]
+		[TestCase(null, TestEnum.AnOtherThing, TestEnum.AnOtherThing)]
+		[TestCase("anotherthing", TestEnum.AThing, TestEnum.AnOtherThing)]
+		public void EnumTest(string value, TestEnum? defaultValue, TestEnum? expected)
 		{
-			var result = value.ToEnum<TestEnum>();
+			TestEnum? result;
+			if (defaultValue.HasValue)
+			{
+				result = value.ToEnum(defaultValue.Value);
+			}
+			else
+			{
+				result = value.ToEnum<TestEnum>();
+			}
 
 			Assert.AreEqual(expected, result);
 		}
